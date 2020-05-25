6fcfbde6413d60fa0e71be9f5ffaf605413d977f
Corentin Forler <cogk42@users.noreply.github.com>
2020-05-25 17:27:47 +0200

    js

--- DASI-TD2-Correction/src/main/webapp/common.js
+++ DASI-TD2-Correction/src/main/webapp/common.js
@@ -35,20 +35,24 @@ function requestApi(params = {}) {
 const wrap = (tag, inner) => `<${tag}>${inner}</${tag}>`
 const ul = inner => wrap('ul', inner)
 const li = inner => wrap('li', inner)
+const tr = inner => wrap('tr', inner)
+const td = inner => wrap('td', inner)
 
 const reformatDateTimeString = (dts) => {
     // dts = date time string, 'YYYY-MM-DD HH:MM:SS' format
     const [date, time] = dts.split(' ')
     const [Y, M, D] = date.split('-')
     const [h, m, s] = time.split(':')
-    const p = s => (s || '??').padStart('0', 2)
+    const p = X => (X || '??').padStart('0', 2)
     return `${p(D)}/${p(M)}/${Y} ${p(h)}h${p(m)}`
 }
 
 // ------------
 // Transformateurs : Objet --> HTML
 /** @param {Medium} m */
-const mediumToHtml = m => `${m.prenom} ${m.nom} (${m.type})`
+const mediumToHtml = m => `${m.denomination} (${m.type})`
+
+const clientToHtml = c => `${c.prenom} ${c.nom}`
 
 /** @param {Medium[]} mediums */
 const mediumsToHtml = mediums => ul(mediums.map(m => li(mediumToHtml(m))).join(''))
@@ -60,9 +64,22 @@ const consultationToTr = consult => {
         reformatDateTimeString(consult.dateFin),
         mediumToHtml(consult.medium)
     ]
-    return wrap('tr', fields.map(x => wrap('td', x)).join('\n'))
+    return tr(fields.map(x => td(x)).join('\n'))
+}
+
+/** @param {Consultation} consult */
+const consultationToTrExtended = consult => {
+    const fields = [
+        reformatDateTimeString(consult.dateDebut),
+        reformatDateTimeString(consult.dateFin),
+        mediumToHtml(consult.medium),
+        clientToHtml(consult.client),
+        consult.commentaire
+    ]
+    return tr(fields.map(x => td(x)).join('\n'))
 }
 
 // Quelques types utiles
-/** @typedef {{name: string, type: string}} Medium */
-/** @typedef {{dateDebut: string, dateFin: string, medium: Medium}} Consultation */
+/** @typedef {{denomination: string, type: string}} Medium */
+/** @typedef {{nom: string, prenom: string}} Client */
+/** @typedef {{dateDebut: string, dateFin: string, medium: Medium, client: Client, commentaire: string}} Consultation */
